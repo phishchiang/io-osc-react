3eec32654ff3aa578514b8ed9f16b11c758799fb
phish <phishchiang@gmail.com>
2019-12-19 17:21:48 +0800

    add the osc js

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express = require("express");
 const socketIO = require("socket.io");
 const path = require("path");
 
-// const OSC = require("osc-js");
-// const config = { udpClient: { port: 9129, secure: true } };
-// const osc = new OSC({ plugin: new OSC.BridgePlugin(config) });
+const OSC = require("osc-js");
+const config = { udpClient: { port: 9129, secure: true } };
+const osc = new OSC({ plugin: new OSC.BridgePlugin(config) });
 
-// osc.open(); // start a WebSocket server on port 8080
+osc.open(); // start a WebSocket server on port 8080
 
-// osc.on("/test/random", message => {
-//   console.log(message.args); // prints the message arguments
-//   console.log("好想睡覺喔");
-// });
+osc.on("/test/random", message => {
+  console.log(message.args); // prints the message arguments
+  console.log("好想睡覺喔");
+});
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, "./public/index.html");
@@ -26,8 +26,6 @@ io.on("connection", socket => {
   console.log("Client connected");
   socket.on("disconnect", () => console.log("Client disconnected"));
 });
-
-setInterval(() => io.emit("time", new Date().toTimeString()), 1000);
 
 io.on("connection", function(socket) {
   console.log(socket.id);
